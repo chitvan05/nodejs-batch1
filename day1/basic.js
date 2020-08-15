93b64ef3fd995dfb066490ce93e1e85f1440b141
Rahul Nalawade <48743273+chitvan05@users.noreply.github.com>
2020-08-15 15:45:13 +0530

    Add files via upload
    basics of javascript 15/08/2020

---
+++ day1/basic.js
@@ -0,0 +1,57 @@
+//variable declarations
+
+var one = 15;
+var string  = "demo string";
+var array = [1,2,3,4,5];
+
+console.log(one); // this statement is use to print 
+
+// If loop in javascript
+console.log(" ///////////////////////");
+var high = 15;
+var low = 10;
+
+if(high > low){
+
+    console.log("\n high value is greater");
+}
+else {
+
+    console.log("\n high value is not greater");
+}
+
+
+//for loop  in javascript
+
+console.log(" ///////////////////////");
+
+var array1= [1,2,3,4,5];
+
+for(var i = 0; i < array1.length; i++){
+
+    console.log("\n in for loop: ",array1[i]);
+
+}
+
+//while loop in javascript
+
+
+console.log(" ///////////////////////");
+var r = 0;
+
+while(r < 5){
+
+    console.log("\n in while loop: ",r);
+    r++
+}
+
+
+// function in javascript
+
+console.log(" ///////////////////////");
+
+function function1(){
+
+    console.log("\n in function: ")
+}
+function1();
